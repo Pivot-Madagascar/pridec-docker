3d8ebef1e80eebd08baeeca976977c03953052be
andrypaul <pandriamanorintsoa@pivotworks.org>
2026-06-15 11:28:01 +0300

    Remove old etlhub app

--- etlhub/api/dependencies.py
+++
@@ -1,7 +0,0 @@
-from functools import lru_cache
-from hubcenter.infrastructure.job_store import JobStore
-
-
-@lru_cache
-def get_job_store() -> JobStore:
-    return JobStore()
--- etlhub/domain/schemas/etl.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class ETLResponse(BaseModel):
-    status: str = ""
-    message: str = ""
-    job_id: str = ""
