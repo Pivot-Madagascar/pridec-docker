fd1651632eb53969e18365fd29c6f6e06681e12d
andrypaul <pandriamanorintsoa@pivotworks.org>
2026-07-23 14:23:55 +0300

    Replace logo SVG with Icon component and add database icon to Icons index

--- etlui/src/App.vue
+++ etlui/src/App.vue
@@ -3,11 +3,9 @@
     <!-- Top Navbar -->
     <header class="app-header">
       <div class="app-header-content">
-        <router-link to="/" class="app-logo">
+<router-link to="/" class="app-logo">
           <div class="logo-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon-md" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4" />
-            </svg>
+            <Icon :path="ICONS.database" class="icon-md" />
           </div>
           <div>
             <h1 class="logo-title">ETL Hub</h1>
